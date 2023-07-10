ae6fd9f328925d94979d0dc43c8f7ecbe388c178
DAndreDerr <dandre020727@gmail.com>
2023-07-10 14:15:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Very Easy
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Employee's information</h1>
+//         <p>Name:Dre</p>
+//         <p>Number:9808758789</p>
+//         <p>Date of Birth: 07/27/2002</p>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+// Easy/Medium
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       person: {
+//         name: " Dre",
+//         number: "9808758789",
+//         DOB: "07/27/2002",
+//       },
+//     };
+//   }
+//   render() {
+//     return <IndividualsInfo person={this.state.person} />;
+//   }
+// }
+
+// class IndividualsInfo extends React.component {
+//   render() {
+//     const { person } = this.props;
+
+//     return (
+//       <div>
+//         <h1>Employee's information</h1>
+//         <p>Name:{person.name}</p>
+//         <p>Number:{person.number}</p>
+//         <p>Date of Birth: {person.DOB}</p>
+//       </div>
+//     );
+//   }
+// }
+
+
+// Hard
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [
+        {
+          name: 'Dre',
+          number: '9808758789',
+          DOB: '07/27/2002'
+        },
+        {
+          name: 'Dre',
+          number: '9808758789',
+          DOB: '07/27/2002'
+        },
+        {
+          name: 'Dre',
+          number: '9808758789',
+          DOB: '07/27/2002'
+        },
+        {
+          name: 'Dre',
+          number: '9808758789',
+          DOB: '07/27/2002'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const { people } = this.state;
+    const employeeData = [];
+
+    for (let i = 0; i < people.length; i++) {
+      const person = people[i];
+      employeeData.push(
+        <div key={i}>
+          <h2>Name: {person.name}</h2>
+          <p>Number: {person.number}</p>
+          <p>Date of Birth: {person.DOB}</p>
+        </div>
+      );
+    }
+
+    return (
+      <div class="background">
+        <h1>Employee's information</h1>
+        {employeeData}
+      </div>
+    );
+  }
 }
+
 
 export default App;
